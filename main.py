b9e0a2164e52172bb01475291efd68f2cf919601
Lakshya Kesrwani <167260069+Lakshya-kesarwani@users.noreply.github.com>
2025-08-30 22:48:32 +0000

    added map and search functionality

--- main.py
+++ main.py
@@ -10,17 +10,22 @@ app = Flask(__name__,static_folder='static')
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+product_hunt_api = os.getenv("PRODUCT_HUNT_API_TOKEN")
 @app.route("/api/products")
 def get_products():
     try:
-        if not PRODUCT_HUNT_API_TOKEN or PRODUCT_HUNT_API_TOKEN == "your_product_hunt_api_token":
+        if not product_hunt_api or product_hunt_api == "your_product_hunt_api_token":
             return jsonify({"error": "Product Hunt API token is missing or invalid. Please check your .env file."}), 500
 
         cursor_position = request.args.get('after')            
         data = get_product_hunt_data(after=cursor_position)
         return jsonify(data)
-    except requests.exceptions.HTTPerror as e:
+    except requests.exceptions.HTTPError as e:
         return jsonify({"error":str(e)}),e.response.status_code
 
 @app.route("/category/<category_name>")
--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-
+import os
+import requests
 load_dotenv()
 
 product_hunt_api = os.getenv("PRODUCT_HUNT_API_TOKEN")
